99f2f4fa8f5720bc525670a18c60a4aaed5f9251
Juliana Silva <118191420+JulianaVanier@users.noreply.github.com>
2023-08-15 21:01:13 +0000

    updated code

--- node_quickstart/index.js
+++ node_quickstart/index.js
@@ -1,18 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require('body-parser')
+
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
 const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.post('/', (req, res) => {
+
+  console.log(req.body);
+
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db('mongodemo');
+      const student = database.collection('student');
+
+
+      // const query = { name: 'Juliana', age: 41 };
+      const result = await student.updateOne(req.body, {$set:{"age":100}});
+      console.log(result);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
   }
-}
-run().catch(console.dir);
+  run().catch(console.dir);
+
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
